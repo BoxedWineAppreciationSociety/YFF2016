c06c8d906d43971c892744d88619fe32e7090ef7
Isaac Norman <isaacdnorman@gmail.com>
2016-01-12 19:44:50 +1100

    Add colours and fonts (as well as centering) to the first launch menu buttons

--- YFF2016/FirstLaunchViewController.swift
+++ YFF2016/FirstLaunchViewController.swift
@@ -5,15 +5,33 @@
 //  Created by Chris Jewell on 7/01/2016.
 //  Copyright © 2016 Yackandandah Folk Festival. All rights reserved.
 //
-
 import UIKit
 
 class FirstLaunchViewController: UIViewController {
 
+    @IBOutlet weak var programButton: UIButton!
+    @IBOutlet weak var mapButton: UIButton!
+    @IBOutlet weak var artistsButton: UIButton!
+    @IBOutlet weak var feedButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Set Button Colors
+        programButton.tintColor = UIColor.whiteColor()
+        mapButton.tintColor = UIColor.whiteColor()
+        artistsButton.tintColor = UIColor.whiteColor()
+        feedButton.tintColor = UIColor.whiteColor()
+        
+        programButton.titleLabel?.font = UIFont(name: "SourceSansPro-Semibold", size: 18)
+        mapButton.titleLabel?.font = UIFont(name: "SourceSansPro-Semibold", size: 18)
+        artistsButton.titleLabel?.font = UIFont(name: "SourceSansPro-Semibold", size: 18)
+        feedButton.titleLabel?.font = UIFont(name: "SourceSansPro-Semibold", size: 18)
+        
+        programButton.backgroundColor = UIColor(red: 199.0/255/0, green: 86.0/255.0, blue: 61.0/255.0, alpha: 1.0)
+        mapButton.backgroundColor = UIColor(red: 228.0/255.0, green: 117.0/255.0, blue: 48.0/255.0, alpha: 1.0)
+        artistsButton.backgroundColor = UIColor(red: 154.0/255.0, green: 155.0/255.0, blue: 82.0/255.0, alpha: 1.0)
+        feedButton.backgroundColor = UIColor(red: 76.0/255.0, green: 133.0/255.0, blue: 121.0/255.0, alpha: 1.0)
     }
 
     override func didReceiveMemoryWarning() {
