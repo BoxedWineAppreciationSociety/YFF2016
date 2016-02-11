0eef475fa59c031460f8d3a106bf876d537036b1
Isaac Norman <isaacdnorman@gmail.com>
2016-02-11 21:38:37 +1100

    Select Day for Program. Now with Animation!

---
+++ YFF2016/programDayButton.swift
@@ -0,0 +1,30 @@
+//
+//  programDayButton.swift
+//  YFF2016
+//
+//  Created by Isaac Norman on 11/02/2016.
+//  Copyright © 2016 Yackandandah Folk Festival. All rights reserved.
+//
+
+import UIKit
+
+class programDayButton: UIButton {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        // Text Color
+        self.tintColor = UIColor.blackColor()
+        
+        // Font
+        self.titleLabel?.font = UIFont(name: "BebasNeue", size: 18)
+        
+        // Border
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(red: 201.0/255.0, green: 201.0/255.0, blue: 203.0/255.0, alpha: 1.0).CGColor
+        
+        //Background
+        self.backgroundColor = UIColor.whiteColor()
+
+    }
+}
