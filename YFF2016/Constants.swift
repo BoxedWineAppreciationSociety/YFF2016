f20017013b3c92d53b6926e94006fce2db74f706
Isaac Norman <isaacdnorman@gmail.com>
2016-02-19 14:23:06 +1100

    Load all Instagram images on launch for speedy feed times

--- YFF2016/Constants.swift
+++ YFF2016/Constants.swift
@@ -10,6 +10,8 @@
 import Foundation
     
 let artistsJsonUrl = "https://raw.githubusercontent.com/RustComet/YFFJSON/master/artists_remote.json"
+    
+let instagramUrl = "https://api.instagram.com/v1/tags/yff15/media/recent?access_token=3212807.1677ed0.e19c549707764af7a71e8a8445e1534d"
 
 let friPerformancesJsonUrl = "https://raw.githubusercontent.com/RustComet/YFFJSON/master/fri_performances_remote.json"
 let satPerformancesJsonUrl = "https://raw.githubusercontent.com/RustComet/YFFJSON/master/sat_performances_remote.json"
