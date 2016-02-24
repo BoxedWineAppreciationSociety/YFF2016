3a039b07a1ebfb19ec76bb9545b86fa92d5bffa2
Isaac Norman <isaacdnorman@gmail.com>
2016-02-24 22:14:00 +1100

    Styled Artist Performance times

---
+++ YFF2016/ArtistPerformanceTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistPerformanceTableViewCell.swift
+//  YFF2016
+//
+//  Created by Isaac Norman on 24/02/2016.
+//  Copyright © 2016 Yackandandah Folk Festival. All rights reserved.
+//
+
+import UIKit
+
+class ArtistPerformanceTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
